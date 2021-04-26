98fa6d9b121c88d619b74a6131f4da7eb16785a2
Jonathan Ross <jross@taeltech.com>
2021-04-26 15:21:08 +0800

    set up project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import * as yup from 'yup';
+import axios from 'axios';
+
+import './App.css';
+import Form from './Form';
+
+function App() {
+  return (
+    <div className="App">
+      <Form />
+    </div>
+  );
+}
+
+export default App;
